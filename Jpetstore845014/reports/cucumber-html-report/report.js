0344b20ee847e70c163fe34064d26517e643cb34
kalyan <kalyan@DESKTOP-BOE9VS2>
2020-03-27 16:13:39 +0530

    commit

--- Jpetstore845014/reports/cucumber-html-report/report.js
+++ Jpetstore845014/reports/cucumber-html-report/report.js
@@ -50,29 +50,27 @@ formatter.match({
   "location": "Registration_test.enter_user_ID_and_password()"
 });
 formatter.result({
-  "duration": 15037708900,
-  "status": "passed"
+  "duration": 17849298100,
+  "error_message": "org.openqa.selenium.SessionNotCreatedException: session not created\nfrom chrome not reachable\n  (Session info: chrome\u003d80.0.3987.149)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-BOE9VS2\u0027, ip: \u0027192.168.43.149\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_242\u0027\nDriver info: driver.version: ChromeDriver\nremote stacktrace: Backtrace:\n\tOrdinal0 [0x00F50C83+1707139]\n\tOrdinal0 [0x00EB68F1+1075441]\n\tOrdinal0 [0x00E2DE72+515698]\n\tOrdinal0 [0x00E27CA8+490664]\n\tOrdinal0 [0x00E2839B+492443]\n\tOrdinal0 [0x00E292F5+496373]\n\tOrdinal0 [0x00E24F05+478981]\n\tOrdinal0 [0x00E2ECA0+519328]\n\tOrdinal0 [0x00DDCA67+182887]\n\tOrdinal0 [0x00DDBDDD+179677]\n\tOrdinal0 [0x00DD9D4B+171339]\n\tOrdinal0 [0x00DC1D4A+73034]\n\tOrdinal0 [0x00DC2DC0+77248]\n\tOrdinal0 [0x00DC2D59+77145]\n\tOrdinal0 [0x00ECBB67+1162087]\n\tGetHandleVerifier [0x00FEA966+508998]\n\tGetHandleVerifier [0x00FEA6A4+508292]\n\tGetHandleVerifier [0x00FFF7B7+594583]\n\tGetHandleVerifier [0x00FEB1D6+511158]\n\tOrdinal0 [0x00EC402C+1130540]\n\tOrdinal0 [0x00ECD4CB+1168587]\n\tOrdinal0 [0x00ECD633+1168947]\n\tOrdinal0 [0x00EE5B35+1268533]\n\tBaseThreadInitThunk [0x759D6359+25]\n\tRtlGetAppContainerNamedObjectPath [0x770E7B74+228]\n\tRtlGetAppContainerNamedObjectPath [0x770E7B44+180]\n\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.W3CHandshakeResponse.lambda$errorHandler$0(W3CHandshakeResponse.java:62)\r\n\tat org.openqa.selenium.remote.HandshakeResponse.lambda$getResponseFunction$0(HandshakeResponse.java:30)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.lambda$createSession$0(ProtocolHandshake.java:126)\r\n\tat java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:193)\r\n\tat java.util.Spliterators$ArraySpliterator.tryAdvance(Spliterators.java:958)\r\n\tat java.util.stream.ReferencePipeline.forEachWithCancel(ReferencePipeline.java:126)\r\n\tat java.util.stream.AbstractPipeline.copyIntoWithCancel(AbstractPipeline.java:499)\r\n\tat java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:486)\r\n\tat java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:472)\r\n\tat java.util.stream.FindOps$FindOp.evaluateSequential(FindOps.java:152)\r\n\tat java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)\r\n\tat java.util.stream.ReferencePipeline.findFirst(ReferencePipeline.java:531)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.createSession(ProtocolHandshake.java:128)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.createSession(ProtocolHandshake.java:74)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:136)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.startSession(RemoteWebDriver.java:213)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.\u003cinit\u003e(RemoteWebDriver.java:131)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:181)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:168)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:123)\r\n\tat com.pages.Registration_page.url(Registration_page.java:40)\r\n\tat com.stepdefinition.Registration_test.enter_user_ID_and_password(Registration_test.java:17)\r\n\tat ✽.Given Enter user ID and password(src/main/resources/feature/testcase.feature:5)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "Registration_test.add_the_account_information()"
 });
 formatter.result({
-  "duration": 1189724400,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Registration_test.save_the_account_inforamtion()"
 });
 formatter.result({
-  "duration": 2630896700,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "Registration_test.complete_the_registration()"
 });
 formatter.result({
-  "duration": 5852457100,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.scenario({
   "line": 11,
@@ -110,216 +108,4 @@ formatter.step({
 });
 formatter.match({
   "location": "Loginmain_test.the_user_launches_the_website_in_chrome_browser()"
-});
-formatter.result({
-  "duration": 15225001500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Loginmain_test.opens_the_login_page()"
-});
-formatter.result({
-  "duration": 3501381500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Loginmain_test.enter_username_and_password()"
-});
-formatter.result({
-  "duration": 30246736100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Loginmain_test.click_on_login_button()"
-});
-formatter.result({
-  "duration": 164358100,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 18,
-  "name": "Search for the animal in the application",
-  "description": "",
-  "id": "jpet-website;search-for-the-animal-in-the-application",
-  "type": "scenario",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "line": 17,
-      "name": "@tc03_Searchmain"
-    }
-  ]
-});
-formatter.step({
-  "line": 19,
-  "name": "the search bar in the login page",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 20,
-  "name": "search for the animal you are looking for",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 21,
-  "name": "enter the product id and name in the search bar",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 22,
-  "name": "click on the Search button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "Mainsearch_test.the_search_bar_in_the_login_page()"
-});
-formatter.result({
-  "duration": 11229912100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Mainsearch_test.search_for_the_animal_you_are_looking_for()"
-});
-formatter.result({
-  "duration": 218569600,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Mainsearch_test.enter_the_product_id_and_name_in_the_search_bar()"
-});
-formatter.result({
-  "duration": 4262286000,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Mainsearch_test.click_on_the_Search_button()"
-});
-formatter.result({
-  "duration": 5173991000,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 25,
-  "name": "Invalid login to the Jpet store application",
-  "description": "",
-  "id": "jpet-website;invalid-login-to-the-jpet-store-application",
-  "type": "scenario",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "line": 24,
-      "name": "@tc04_invalidlogin"
-    }
-  ]
-});
-formatter.step({
-  "line": 26,
-  "name": ":Open the jpet website",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 27,
-  "name": ":opens the next login page",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 28,
-  "name": ":enter invalid username and invalid password",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 29,
-  "name": ":click on login click button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "Invalidlogin_test.open_the_jpet_website()"
-});
-formatter.result({
-  "duration": 15768081400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Invalidlogin_test.opens_the_next_login_page()"
-});
-formatter.result({
-  "duration": 2595526400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Invalidlogin_test.enter_invalid_username_and_invalid_password()"
-});
-formatter.result({
-  "duration": 388686800,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Invalidlogin_test.click_on_login_click_button()"
-});
-formatter.result({
-  "duration": 8685691100,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 32,
-  "name": "Select the animal and add to cart",
-  "description": "",
-  "id": "jpet-website;select-the-animal-and-add-to-cart",
-  "type": "scenario",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "line": 31,
-      "name": "@tc05_addtocart"
-    }
-  ]
-});
-formatter.step({
-  "line": 33,
-  "name": "user launches the website in browser",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 34,
-  "name": "opens the signin page",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 35,
-  "name": "click on cat logo and select one of them",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 36,
-  "name": "add product to the cart and proceed to checkout",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "Addtocart_test.user_launches_the_website_in_browser()"
-});
-formatter.result({
-  "duration": 15132178700,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Addtocart_test.opens_the_signin_page()"
-});
-formatter.result({
-  "duration": 7829027300,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Addtocart_test.click_on_cat_logo_and_select_one_of_them()"
-});
-formatter.result({
-  "duration": 3340898400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "Addtocart_test.add_product_to_the_cart_and_proceed_to_checkout()"
-});
-formatter.result({
-  "duration": 6109240300,
-  "status": "passed"
-});
 });
